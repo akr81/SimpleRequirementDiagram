3798f27e4f4b98d771ef3745f7b1a3a191cd1545
akr81 <akira21120903@gmail.com>
2022-09-18 17:00:24 +0900

    Improve target related nodes extraction.

--- make_requirement_diagram.py
+++ make_requirement_diagram.py
@@ -7,7 +7,8 @@ from src.convert_puml_code import ConvertPumlCode
 
 @click.command()
 @click.option("-r", "--req", help="path to the requirements json file", required=True)
-def main(req):
+@click.option("-t", "--target", help="target node as unique_id")
+def main(req, target):
     with open(req, "r", encoding="UTF-8") as f:
         requirements = json.load(f)
     manager = ManageGraph()
@@ -22,6 +23,10 @@ def main(req):
         f.writelines(puml)
 
     subprocess.run(["plantuml", "sample.puml"])
+
+    if target:
+        related_nodes = manager.get_target_connected_nodes(graph, target, 500, 500)
+        print(related_nodes)
 
 
 if __name__ == "__main__":
--- src/manage_graph.py
+++ src/manage_graph.py
@@ -45,3 +45,42 @@ class ManageGraph:
                     )
 
         return graph
+
+    def get_target_connected_nodes(
+        self, graph: nx.DiGraph, target_id: str, up_level: int, down_level
+    ) -> List[str]:
+        # Get upper nodes
+        upper_nodes = self._get_upper_nodes(graph, target_id, up_level)
+
+        # Get lower nodes
+        lower_nodes = self._get_lower_nodes(graph, target_id, down_level)
+
+        print(upper_nodes)
+        print(lower_nodes)
+        return list(set(upper_nodes + lower_nodes))
+
+    def _get_upper_nodes(
+        self, graph: nx.DiGraph, target_id: str, up_level: int
+    ) -> List[str]:
+        upper_nodes = [target_id]
+        for _ in range(up_level):
+            temp_upper_nodes = []
+            for upper_node in upper_nodes:
+                for node in graph.successors(upper_node):
+                    temp_upper_nodes.append(node)
+            upper_nodes = list(set(upper_nodes + temp_upper_nodes))
+
+        return upper_nodes
+
+    def _get_lower_nodes(
+        self, graph: nx.DiGraph, target_id: str, down_level: int
+    ) -> List[str]:
+        lower_nodes = [target_id]
+        for _ in range(down_level):
+            temp_lower_nodes = []
+            for upper_node in lower_nodes:
+                for node in graph.predecessors(upper_node):
+                    temp_lower_nodes.append(node)
+            lower_nodes = list(set(lower_nodes + temp_lower_nodes))
+
+        return lower_nodes
